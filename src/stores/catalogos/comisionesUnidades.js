b1558a86af30476b9028853e352619b1f1121f56
Luis Solano <lsolano@gruver.mx>
2024-06-13 16:47:41 -0600

    Se agrega automaticamente la previa a las facturas

--- src/stores/catalogos/comisionesUnidades.js
+++ src/stores/catalogos/comisionesUnidades.js
@@ -1,40 +1,60 @@
-import { defineStore, storeToRefs } from 'pinia'
-import { api } from 'src/boot/axios'
-import { notificacion } from 'src/helpers/mensajes'
-import { ref } from 'vue'
+import { defineStore, storeToRefs } from "pinia";
+import { api } from "src/boot/axios";
+import { notificacion } from "src/helpers/mensajes";
+import { ref } from "vue";
 
-export const useComisionesStore = defineStore('comisiones', () => {
-
-  const comisionesUnidades = ref([])
-  const comisionesFiltradas = ref([])
-  const cargando = ref(false)
+export const useComisionesStore = defineStore("comisiones", () => {
+  const comisionesUnidades = ref([]);
+  const comisionesFiltradas = ref([]);
+  const cargando = ref(false);
 
   const obtenerComisionesUnidades = async (objComisiones) => {
     try {
-      cargando.value = true
-      const { data } = await api.post('/unidades/autos', objComisiones)
-      comisionesUnidades.value = [...data]
+      cargando.value = true;
+      const { data } = await api.post("/unidades/autos", objComisiones);
+      comisionesUnidades.value = [...data];
+
+      for (const comision of comisionesUnidades.value) {
+        if (comision.modelo.includes("AVEO")) {
+          comision.descuentosUnidades[0] = {
+            previa: 1242,
+          };
+        } else if (
+          comision.modelo.includes("S10") ||
+          comision.modelo.includes("CAPTIVA") ||
+          comision.modelo.includes("GROOVE") ||
+          comision.modelo.includes("TORNADO")
+        ) {
+          comision.descuentosUnidades[0] = {
+            previa: 1142,
+          };
+        } else {
+          comision.descuentosUnidades[0] = {
+            previa: 693,
+          };
+        }
+      }
     } catch (error) {
-      console.log(error)
-    }finally{
-      cargando.value = false
+      console.log(error);
+    } finally {
+      cargando.value = false;
     }
-  }
+  };
 
   const guardarDescuentos = async (objDescuentos) => {
     try {
-      const { data } = await api.post('/descuentos/autos', objDescuentos)
-      notificacion('positive', 'Descuentos Agregados', data)
+      const { data } = await api.post("/descuentos/autos", objDescuentos);
+      notificacion("positive", "Descuentos Agregados", data);
     } catch (error) {
-      notificacion('negative', error.response.data.message)
+      notificacion("negative", error.response.data.message);
     }
-  }
+  };
 
   return {
-      comisionesUnidades,
-      comisionesFiltradas,
-      cargando,
-      obtenerComisionesUnidades,
-      guardarDescuentos
-    }
-})
+    comisionesUnidades,
+    comisionesFiltradas,
+    cargando,
+    obtenerComisionesUnidades,
+    guardarDescuentos,
+  };
+});
